ad2771a457adb8880b0a63420061500d3315f823
丫P <2269940343@qq.com>
2020-04-28 20:59:08 +0800

    五一作业测试

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <p>1.本项目会自动把px自动转换成rem，不需要考虑移动端兼容性问题，单位直接用px即可，浏览器上会自动变成是rem;若不想把px转换成rem，那么直接在对应的css属性后面增加一个/*no*/</p>
-    <p>zwt上传分支</p>
+    <p>导航栏页面</p>
+    <p>1111</p>
     <router-view />
   </div>
 </template>
